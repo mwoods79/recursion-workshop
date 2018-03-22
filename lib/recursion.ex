67970d29e2a39e7054290b1aa8dc4d85cab6d767
Micah Woods <micahwoods@gmail.com>
2018-03-22 11:51:35 -0400

    Add solutions to workshop problems

--- lib/recursion.ex
+++ lib/recursion.ex
@@ -6,6 +6,73 @@ defmodule Recursion do
   def b_not(true), do: false
   def b_not(false), do: true
 
+  def b_or(true, _false), do: true
+  def b_or(_true, true), do: true
+  def b_or(_first, _second), do: false
+
+  def b_and(true, true), do: true
+  def b_and(_first, _second), do: false
+
   def count([]), do: 0
   def count([_h | t]), do: 1 + count(t)
+
+  def maximum([h]), do: h
+
+  def maximum([h | tail]) do
+    max(h, maximum(tail))
+  end
+
+  def remove_dups(list) when is_list(list) do
+    remove_dups(list, [])
+  end
+
+  defp remove_dups([h | t1], [h | _t2] = acc) do
+    remove_dups(t1, acc)
+  end
+
+  defp remove_dups([h | t], acc) do
+    remove_dups(t, [h | acc])
+  end
+
+  defp remove_dups([], acc) do
+    Enum.reverse(acc)
+  end
+
+  def zip(_, []), do: []
+  def zip([], _), do: []
+  def zip([h1 | t1], [h2 | t2]), do: [{h1, h2} | zip(t1, t2)]
+
+  def reduce([], init, _fun), do: init
+  def reduce([head | tail], init, fun), do: fun.(head, reduce(tail, init, fun))
+
+  def reduce([head | tail], fun) do
+    reduce(tail, head, fun)
+  end
+
+  def foldl([], init, _fun), do: init
+  def foldl([head | tail], init, fun), do: foldl(tail, fun.(init, head), fun)
+
+  def map(list, fun) do
+    reduce(list, [], fn x, acc -> [fun.(x) | acc] end)
+  end
+
+  def filter(list, fun) when is_list(list) do
+    filter_fun = fn x, acc ->
+      if fun.(x) do
+        [x | acc]
+      else
+        acc
+      end
+    end
+
+    reduce(list, [], filter_fun)
+  end
+
+  def quicksort([]), do: []
+
+  def quicksort([pivot | rest]) do
+    lesser = filter(rest, fn i -> i <= pivot end)
+    greater = filter(rest, fn i -> i > pivot end)
+    quicksort(lesser) ++ [pivot] ++ quicksort(greater)
+  end
 end
--- test/recursion_test.exs
+++ test/recursion_test.exs
@@ -7,62 +7,62 @@ defmodule RecursionTest do
     assert b_not(false) == true
   end
 
-  # test "boolean logic (or)" do
-  #   assert b_or(true, false) == true
-  #   assert b_or(false, true) == true
-  #   assert b_or(false, false) == false
-  # end
+  test "boolean logic (or)" do
+    assert b_or(true, false) == true
+    assert b_or(false, true) == true
+    assert b_or(false, false) == false
+  end
 
-  # test "boolean logic (and)" do
-  #   assert b_and(true, true) == true
-  #   assert b_and(false, true) == false
-  #   assert b_and(true, false) == false
-  #   assert b_and(false, false) == false
-  # end
+  test "boolean logic (and)" do
+    assert b_and(true, true) == true
+    assert b_and(false, true) == false
+    assert b_and(true, false) == false
+    assert b_and(false, false) == false
+  end
 
   test "count" do
     assert count(["a", "b", "c"]) == 3
   end
 
-  # test "maximum" do
-  #   list = [5, 4, 9, 2, 7]
-  #   assert maximum(list) == 9
-  # end
+  test "maximum" do
+    list = [5, 4, 9, 2, 7]
+    assert maximum(list) == 9
+  end
 
-  # test "remove dups" do
-  #   list = [1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 5]
-  #   assert remove_dups(list) == [1, 2, 3, 4, 5]
-  # end
+  test "remove dups" do
+    list = [1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 5]
+    assert remove_dups(list) == [1, 2, 3, 4, 5]
+  end
 
-  # test "zip" do
-  #   assert zip([], [1, 2, 3]) == []
-  #   assert zip([1, 2, 3], []) == []
-  #   assert zip([1, 2, 3], [1, 2, 3]) == [{1, 1}, {2, 2}, {3, 3}]
-  #   assert zip([1, 2, 3], [1, 2, 3, 4]) == [{1, 1}, {2, 2}, {3, 3}]
-  # end
+  test "zip" do
+    assert zip([], [1, 2, 3]) == []
+    assert zip([1, 2, 3], []) == []
+    assert zip([1, 2, 3], [1, 2, 3]) == [{1, 1}, {2, 2}, {3, 3}]
+    assert zip([1, 2, 3], [1, 2, 3, 4]) == [{1, 1}, {2, 2}, {3, 3}]
+  end
 
-  # test "reduce" do
-  #   assert reduce([1, 2, 3], 0, fn x, total -> x + total end) == 6
-  # end
+  test "reduce" do
+    assert reduce([1, 2, 3], 0, fn x, total -> x + total end) == 6
+  end
 
-  # test "reduce (helper)" do
-  #   assert reduce([1, 2, 3], fn x, total -> x + total end) == 6
-  # end
+  test "reduce (helper)" do
+    assert reduce([1, 2, 3], fn x, total -> x + total end) == 6
+  end
 
-  # test "foldl" do
-  #   assert foldl([1, 2, 3], 0, fn x, total -> x + total end) == 6
-  # end
+  test "foldl" do
+    assert foldl([1, 2, 3], 0, fn x, total -> x + total end) == 6
+  end
 
-  # test "map" do
-  #   assert map([1, 2, 3], fn x -> x * 2 end) == [2, 4, 6]
-  # end
+  test "map" do
+    assert map([1, 2, 3], fn x -> x * 2 end) == [2, 4, 6]
+  end
 
-  # test "filter" do
-  #   assert filter([1, 2, 3, 4], fn x -> rem(x, 2) == 0 end) == [2, 4]
-  # end
+  test "filter" do
+    assert filter([1, 2, 3, 4], fn x -> rem(x, 2) == 0 end) == [2, 4]
+  end
 
-  # test "quicksort" do
-  #   list = [8, 2, 5, 3, 5, 1, 9, 0, 6, 5]
-  #   assert quicksort(list) == [0, 1, 2, 3, 5, 5, 5, 6, 8, 9]
-  # end
+  test "quicksort" do
+    list = [8, 2, 5, 3, 5, 1, 9, 0, 6, 5]
+    assert quicksort(list) == [0, 1, 2, 3, 5, 5, 5, 6, 8, 9]
+  end
 end
